8b249669a39cab83f4f34d45e838623b02f5ad66
Darragh Mahns <darraghmahns@gmail.com>
2025-08-05 13:31:38 -0600

    fix user registration with custom user model

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import User
 
 class UserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -13,3 +13,21 @@ class UserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'role', 'password1', 'password2')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['email']  # Use email as username
+        user.first_name = self.cleaned_data.get('first_name', '')
+        user.last_name = self.cleaned_data.get('last_name', '')
+        
+        # Set the role
+        role = self.cleaned_data.get('role')
+        if role == 'provider':
+            user.is_provider = True
+        else:
+            user.is_provider = False
+            
+        if commit:
+            user.save()
+        return user
